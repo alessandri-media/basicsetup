520333488d88b31643e0e4df6339976de6430a18
René Alessandri <rene@alessandri-media.ch>
2021-05-21 10:48:03 +0200

    [TASK] Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Extension Manager/Repository config file for ext "basicsetup".
+ */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Basic Setup',
+    'description' => 'Basic setup and configuration for TYPO3 CMS',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-10.4.99',
+            'fluid_styled_content' => '10.4.0-10.4.99',
+            'rte_ckeditor' => '10.4.0-10.4.99'
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'AlessandriMedia\\basicsetup\\' => 'Classes'
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'René Alessandri',
+    'author_email' => 'rene@alessandri-media.ch',
+    'author_company' => 'Alessandri Media',
+    'version' => '0.0.1',
+];
